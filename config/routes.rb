64ecdd3a0d105431770a2b6eebb4d250a89a7504
Yurii.K <yurii.krupa.dev@gmail.com>
2017-05-20 19:40:06 +0300

    Try modyficate device to add Full_name into forms.[Not smart enough. To do in future]

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :trips
   resources :buses
   resources :tickets
-  devise_for :users
+  devise_for :users, controllers: {
+        registrations: 'registration/registrations'
+      }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static#index'
